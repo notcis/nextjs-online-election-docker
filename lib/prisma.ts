2e941f89b11f1a6c3513a677386db425d09a6ba8
notcis <notcis07@gmail.com>
2026-01-26 21:34:30 +0700

    re

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,10 +3,10 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST || "209.97.164.21",
-  user: process.env.DATABASE_USER || "notcis",
-  password: process.env.DATABASE_PASSWORD || "coop2012",
-  database: process.env.DATABASE_NAME || "election_online",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   connectionLimit: 5,
 });
 const prisma = new PrismaClient({ adapter });

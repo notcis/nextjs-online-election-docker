0e2ea5a8d67f94d1f317ebbcd7998b51ec415e8e
notcis <notcis07@gmail.com>
2026-01-26 23:50:10 +0700

    hope

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,5 +3,13 @@ import "dotenv/config";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_SERVER_URL,
+
+  // หากคุณใช้ Trusted Origins (จำเป็นในบางกรณีบน Vercel)
+  trustedOrigins: [
+    process.env.NEXT_PUBLIC_SERVER_URL!,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+  ],
 });

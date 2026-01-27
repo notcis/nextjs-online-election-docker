db4933b3a6fc2427fb1c7f097fbbec025171ec69
notcis <notcis07@gmail.com>
2026-01-27 08:28:23 +0700

    auth fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,9 +3,8 @@ import "dotenv/config";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_SERVER_URL,
-
-  // หากคุณใช้ Trusted Origins (จำเป็นในบางกรณีบน Vercel)
+  baseURL:
+    typeof window !== "undefined"
+      ? window.location.origin
+      : process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
 });
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
@@ -11,4 +12,10 @@ export const auth = betterAuth({
     enabled: true,
   },
   plugins: [nextCookies()],
+  baseURL:
+    process.env.BETTER_AUTH_URL || process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000",
+
+  secret: process.env.BETTER_AUTH_SECRET,
 });

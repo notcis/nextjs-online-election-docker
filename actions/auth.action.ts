941693555750cae55434fcf96072c774a7d2f4ed
notcis <notcis07@gmail.com>
2026-01-29 15:23:40 +0700

    secure: false,

--- actions/auth.action.ts
+++ actions/auth.action.ts
@@ -98,7 +98,8 @@ export async function verifyMemberStatus(
 
     (await cookies()).set("voter_session", member.id, {
       httpOnly: true, // ป้องกัน JavaScript ฝั่ง Client อ่าน Cookie นี้ (กัน XSS)
-      secure: process.env.NODE_ENV === "production", // บังคับใช้ HTTPS บน Production
+      //secure: process.env.NODE_ENV === "production", // บังคับใช้ HTTPS บน Production
+      secure: false,
       sameSite: "lax", // ป้องกันการโจมตีแบบ CSRF
       maxAge: oneHour,
       path: "/",
